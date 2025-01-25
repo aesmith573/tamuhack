229dfe6736069759b301180c209e7c055c4f202f
JacksonWeibelDev <jacksonweibel@tamu.edu>
2025-01-25 16:23:22 -0600

    Budget Page

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
@@ -28,7 +35,7 @@ def signup():
         
         if User.query.filter_by(email=email).first():
            flash('Email already exists.', 'danger')
-           return redirect(url_for('signup'))
+           return redirect(url_for('login'))
 
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         new_user = User(username=username, email=email, password=hashed_password)
@@ -48,20 +55,32 @@ def login():
 
         user = User.query.filter_by(email=email).first()
         if user and check_password_hash(user.password, password):
-            session['user_id'] = user.id
-            session['username'] = user.username
-            flash('Logged in successfully!', 'success')
-            return redirect(url_for('home'))
+            login_user(user)
+            # current_user.id = user.id
+            # current_user.username = user.username
+            return redirect(url_for('profile'))
         else:
             flash('Invalid email or password.', 'danger')
 
     return render_template('login.html')
 
+@login_required
 @app.route('/logout')
 def logout():
+    logout_user()
     session.clear()
     flash('Logged out successfully!', 'success')
     return redirect(url_for('login'))
+
+
+@login_required
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', user=current_user.username)
+
+@app.route('/setbudget')
+def setBudget():
+    return render_template('askForm.html')
 
 if __name__ == '__main__':
     # db.create_all()
