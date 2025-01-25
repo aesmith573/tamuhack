58b2dbab6636992dc48ad48c09e00b15e112af95
JacksonWeibelDev <jacksonweibel@tamu.edu>
2025-01-25 13:02:07 -0600

    Create app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#To run in console
+#flask --app app run
+
+#pip install requests
+
 @app.route("/")
 def hello_world():
     return "<p>Tamu Hack</p>"
